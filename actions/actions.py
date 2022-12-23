77d9b688abb87a794b20f4265b605b1b26d8df0b
Suelen Peres <suelen.prs@gmail.com>
2022-12-23 17:46:10 -0300

    acessando api

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,33 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+from rasa_sdk.events import SlotSet
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "moeda_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        moeda = tracker.get_slot("moeda")
+        url = f'https://economia.awesomeapi.com.br/json/last/BRL-{moeda}'
+
+        json_data = requests.get(url).json()
+        nome_moeda = json_data.get('url')
+
+        print(nome_moeda)
+
+        resp = f'moeda solicitada: {json_data}'
+
+        dispatcher.utter_message(text=resp)
+
+        return [SlotSet("moeda", url)]
